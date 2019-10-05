1171bf0c7b1f6314c304a808c9641352500a12bc
Kamil Ciepiela <kamil_ciepiela@interia.pl>
2019-10-05 12:15:19 +0200

    External classes.

---
+++ Project1/src/klasyzewnetrzne/Main.java
@@ -0,0 +1,21 @@
+package klasyzewnetrzne;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+
+
+    }
+}
+
+class A
+{
+    class B //klasa zagnieżdzona
+    {
+
+    }
+
+}
