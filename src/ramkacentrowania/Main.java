12eceb955be184d5510b4c279587c61ead00a0a5
Kamil Ciepiela <kamil_ciepiela@interia.pl>
2019-10-13 21:57:06 +0200

    Frame in the middle.

---
+++ src/ramkacentrowania/Main.java
@@ -0,0 +1,32 @@
+package ramkacentrowania;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame
+{
+    public Main()
+    {
+        int szerokosc = Toolkit.getDefaultToolkit().getScreenSize().width;
+        int wysokosc = Toolkit.getDefaultToolkit().getScreenSize().height;
+
+        System.out.println(szerokosc);
+        System.out.println(wysokosc);
+
+        this.setSize(szerokosc/2, wysokosc/2);
+
+//        this.setLocation(szerokosc/4, wysokosc/4);
+        int szerokoscRamki = this.getSize().width;
+        int wysokoscRamki = this.getSize().height;
+        this.setLocation((szerokosc-szerokoscRamki)/2, (wysokosc-wysokoscRamki)/2); //wyrażenie uniwaersalne, niezależne od wielkości ramki i rozdzielczości
+
+
+        this.setDefaultCloseOperation(3);
+    }
+
+    public static void main(String[] args)
+    {
+
+        new Main().setVisible(true);
+    }
+}
