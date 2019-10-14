b5d383895b106e83f5b583f8fe7605e9c8973481
KamilCiepiela <kamil_ciepiela@interia.pl>
2019-10-14 15:13:35 +0200

    Frame - template.

---
+++ src/ramkatemplate/Main.java
@@ -0,0 +1,33 @@
+package ramkatemplate;
+
+import javax.swing.*;
+
+public class Main extends JFrame
+{
+    public Main()
+    {
+        super("Zdarzenia");
+        this.setBounds(300,300,300,200);
+
+        initComponents();
+
+        this.setDefaultCloseOperation(3);
+
+    }
+
+    public void initComponents()
+    {
+
+
+
+    }
+
+    public static void main(String[] args)
+    {
+        new Main().setVisible(true);
+
+    }
+
+
+
+}
