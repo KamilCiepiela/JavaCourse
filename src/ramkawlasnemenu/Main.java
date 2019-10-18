2011a25569309a23b727e6f9d843b20606183504
KamilCiepiela <kamil_ciepiela@interia.pl>
2019-10-18 14:48:45 +0200

    Frame - My first menu 2.

--- src/ramkawlasnemenu/Main.java
+++ src/ramkawlasnemenu/Main.java
@@ -2,10 +2,7 @@ package ramkawlasnemenu;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.*;
 
 public class Main extends JFrame
 {
@@ -34,32 +31,27 @@ public class Main extends JFrame
     MenuButton menu1 = new MenuButton("1. Dodaj");
     MenuButton menu2 = new MenuButton("2. Usuń");
     MenuButton menu3 = new MenuButton("3. Zmień");
+    private int i = 0;
 
-    private class MenuButton extends JButton implements FocusListener
+    private class MenuButton extends JButton implements FocusListener, ActionListener
     {
         public MenuButton(String nazwa)
         {
             super(nazwa);
             this.addFocusListener(this);
-            this.addKeyListener(new KeyListener() {
-                /*gdy kliknę np. literę a to wywpłuwane są w kolejności:
-                - keyPressed,
+//            this.addKeyListener(new KeyListener() { // KeyListener to interfejs
+
+                /*gdy kliknę np. literę a to wywoływane są w kolejności:
+                - keyPressed
                 - keyTyped (tylko dla tych, które zostały wydrukowane, np. strzałki nie są wydrukowane i nie zostawiają "śladu" na ekranie,
                 - keyRelease
                 */
-                @Override
-                public void keyTyped(KeyEvent e) {
-
-                }
-
+            this.addActionListener(this);
+            this.addKeyListener(new KeyAdapter() {
+                //klasa adaptacyjna - zadaptowała wszystkie klasy interfejsu, dzięki czemu nie muszę implementować tych wszystkich klas, tylko wybrane, i mogę je nadpisywać
                 @Override
                 public void keyPressed(KeyEvent e) {
-
-                }
-
-                @Override
-                public void keyReleased(KeyEvent e) {
-
+                    keyPressedHandler(e);
                 }
             });
             this.setBackground(kDefault);
@@ -74,8 +66,39 @@ public class Main extends JFrame
         public void focusLost(FocusEvent e) {
             this.setBackground(kDefault);
         }
+
+        private void keyPressedHandler(KeyEvent e){
+            int dlMenu = panelMenu.getComponentCount();
+
+            if (i==0) i = dlMenu; //dzięki temu rozwiązaniu zapętliliśmy i, którę nie osiągnie wartości ujemnych (wyatrczy samo "dlMenu")
+            if (e.getKeyCode() == KeyEvent.VK_DOWN){
+//                System.out.println(++i); //może być tak lub tak jak poniżej
+                panelMenu.getComponent(++i % dlMenu).requestFocus();
+            }
+
+            else if (e.getKeyCode() == KeyEvent.VK_UP){
+//                System.out.println(--i); //może być tak lub tak jak poniżej
+                panelMenu.getComponent(--i % dlMenu).requestFocus();
+            }
+
+            else if (e.getKeyCode() == KeyEvent.VK_ENTER){
+                MenuButton tmp = (MenuButton)e.getSource();  //źródło jest zwracane jako tryb obiektowy, dlategotrzeba je rzutować na MenuButton
+                //to wyrażenie daje dostęp do obiektu, który wywołał obiekt keyPressedHandler
+//                tmp.doClick();
+                ((MenuButton)e.getSource()).doClick(); //można to zrobić szybciej, WARTO TO ZAPAMIĘTAĆ!!!
+
+            }
+
+        }
+
         private Color kFocusGained = Color.RED;
         private Color kDefault = Color.BLUE;
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JOptionPane.showMessageDialog(this, ((MenuButton)e.getSource()).getText());
+        }
+
     }
 
 
