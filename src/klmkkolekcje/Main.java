f5a7bec327684f117fb4e7d9a0e83a7bd18076ed
Kamil Ciepiela <kamil_ciepiela@interia.pl>
2019-12-27 22:16:38 +0100

    Collections - Collection - intro.

---
+++ src/klmkkolekcje/Main.java
@@ -0,0 +1,49 @@
+package klmkkolekcje;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        Collection a; // pojemnik do przechowywania JAKICHKOLWIEK obiektów; wyróżniamy np. zbiór, listę, mapę
+        ArrayList b; //lista rozszerza kolekcję
+        /*lista to bardziej wyspecjalizowany typ kolekcji
+        * przechowuje elementy, np. obiekty; umożliwia wykonanie na nich działań i akcji; dane są w ustalonej kolejności, jeden po drugim;
+        * NIE MA ŻADNYCH PRZERW, zawsze jest jakiś element po innym, z wyjątkiem ostatniego elementu w liście
+        * każdy element w liście ma przypisany adres
+        * o liście można powiedzieć, że INACZEJ JEST TO CIĄG
+        *
+        * ZBIÓR - pojęcie pierwotne, którego nie definiujemy, TEŻ POJEMNIK/""""KOLEKCJA""""
+        * W zbiorze NIE jest ważna KOLEJNOŚĆ, ale jego ZAWARTOŚĆ, jest NIEUPORZĄDKOWANA
+        *
+        * A = {1,1,1,1}  // elemnty powtarzające się nie wpływają na zawartość zbioru!!!!
+        * B = {1}
+        * A = B
+        *
+        * A = {2,4,5}  //  kolejność w zbiorze nie jest ważna!!!!
+        * B = {4,5,2}
+        * A = B
+        *
+        * C = {a,b}
+        * D = {c,d,  e}
+        *
+        * P(a,c); G(b,e)
+        *
+        * mapping - PRZYPORZĄDKOWANIE
+        * KEY - VALUE
+        * ID 1 - "Ania"
+        * ID 2 - "Mira"
+        * ID 3 - "Blanka"
+        * ID ...-
+        *
+         */
+
+        Map c;  // kolekcja, która przyporządkowywuje określonym kluczom (liście) odpowiednie wartości (kolekcje)
+
+
+    }
+}
