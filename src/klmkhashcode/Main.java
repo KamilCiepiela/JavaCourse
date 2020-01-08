7dfd773927e3656c28ee1f6d849fc584c85e57f2
Kamil Ciepiela <kamil_ciepiela@interia.pl>
2020-01-08 15:29:41 +0100

    Codes - Hashcode_3.

--- src/klmkhashcode/Main.java
+++ src/klmkhashcode/Main.java
@@ -1,6 +1,9 @@
 package klmkhashcode;
 
-// hashcode - funkcja mieszająca
+// hashcode to funkcja mieszająca, wyrażona w postaci liczby całkowitej
+// hash musi być taki sam dla równych sobie obiektów
+
+import java.util.Objects;
 
 class Element
 {
@@ -10,14 +13,12 @@ class Element
     }
 
     @Override
-    public boolean equals(Object obj)  // taką metodę powinniśmy ZAWSZE nadpisywać, jeśli jej elementy będą używane później w innym celu niż default
+    public boolean equals(Object obj)  // taką metodę powinniśmy ZAWSZE nadpisywać, jeśli jej elementy będą używane później w innym celu niż default w zbiorze HASH
     {
 //        if (obj == null) return false;  // zabezpieczamy się przed wpisaniem NULL
 
         if (obj == null || this.getClass() != obj.getClass()) return false;  // rónież jeśli klasa obiektu przysłanego jest różna niż klasa Element, to FALSE
-
         if (this == obj) return true;  //dla przyspieszenia działąnia metody - jeśli obiekt porównywany to to samo miejsce, zwróć TRUE
-
         return this.wartosc == ((Element)obj).wartosc;  // przy większej ilości parametrów dodajemy koninkcję (i w tym samym momencie spełnia warunek...)
 
 //        if (this.wartosc == ((Element)obj).wartosc)  //ewentualnie tak można zapisać
@@ -26,7 +27,50 @@ class Element
 //            return false;
     }
 
+//    @Override
+//    public boolean equals(Object obj)  // taką metodę powinniśmy ZAWSZE nadpisywać, jeśli jej elementy będą używane później w innym celu niż default w zbiorze HASH
+//    {
+////        if (obj == null) return false;  // zabezpieczamy się przed wpisaniem NULL
+//
+//        if (obj == null || this.getClass() != obj.getClass()) return false;  // rónież jeśli klasa obiektu przysłanego jest różna niż klasa Element, to FALSE
+//        if (this == obj) return true;  //dla przyspieszenia działąnia metody - jeśli obiekt porównywany to to samo miejsce, zwróć TRUE
+//        return this.wartosc == ((Element)obj).wartosc;  // przy większej ilości parametrów dodajemy koninkcję (i w tym samym momencie spełnia warunek...)
+//
+////        if (this.wartosc == ((Element)obj).wartosc)  //ewentualnie tak można zapisać
+////            return true;
+////        else
+////            return false;
+//    }
+
+//    @Override  // metoda wygenerowana automatycznie przez Intellij
+//    public int hashCode()
+//    {
+//        return Objects.hash(wartosc);
+//    }
+
+    @Override  // metoda zgodna z kursem, takie nadpisanie jest proste dla int, ale dla innych typów niekoniecznie
+    public int hashCode()
+    {
+        int hash = 7;
+        hash = 14 * hash + this.wartosc;  // jeśli dodajemy wartość do wyniku, to hashCode będzie równy, niezależnie od lokalizacji adresu w pamięci
+        /*
+        hash = 89 * hash + (int)(Double.doubleToLongBits(this.cos) ^ (Double.doubleToLongBits(this.cos) >>> 32));
+                // rzutowanie na int, bo hash jest intem (liczbą całkowitą)!!
+                // doubleToLingBits, żeby nie tracić precyzji przy rzutowaniu do inta
+                // na koniec są zmieniane do 32 bitów, żeby się zmieściły w int (double ma 64 bity)
+        hash = 89 * hash + (int) (this.cos2 ^ (this.cos2) >>> 32);  // typ long - podobnie jak double powyżej
+        hash = 89 * hash + (this.cos3 != null ? this.cos3.hashCode() : 0);
+                // typ String - jeśli nie jest nullem, to zwracamy hashCode Stringa (String ma własny hashCode)
+        * TO POWYŻEJ JEST NADMIERNE!!!
+        */
+        return hash;
+    }
+
     int wartosc;
+
+    double cos;
+    long cos2;
+    String cos3;
 }
 
 public class Main
@@ -43,6 +87,13 @@ public class Main
 //        System.out.println(a.equals(b));  // również FLASE, bo sprawdza adresy w taki sam sposób jak poprzednio, nie wartości
 
         System.out.println(a.equals(b));  // po nadpisaniu metody powyżej - TRUE
+
+        System.out.println("abc".hashCode());  // String automatycznie tworzy własny hachCode
+        System.out.println("ac".hashCode());
+        System.out.println();
+
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
 
     }
 }
